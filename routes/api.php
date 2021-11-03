416d1298e5e1d5992109440c5b8a8f7ea156b3f4
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-11-03 12:56:21 -0600

    Creacion de las rutas de la api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+use App\Http\Controllers\API\ProveedoresController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('proveedores',[ProveedoresController::class,'index']);
+Route::post('proveedores',[ProveedoresController::class,'store']);
+Route::put('proveedores/{proveedores}',[ProveedoresController::class,'update']);
+Route::get('proveedores/{proveedores}',[ProveedoresController::class,'show']);
+Route::delete('proveedores/{proveedores}',[ProveedoresController::class,'destroy']);
