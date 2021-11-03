4ff058b5d4e282fc9deb2507da341be4c606b8fe
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-11-03 12:48:18 -0600

    Creacion de ma migracion y tabla proveedores

---
+++ database/migrations/2021_11_02_170933_create_proveedores_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProveedoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proveedores', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombres');
+            $table->string('apellidos');
+            $table->string('direccion');
+            $table->string('ciudad');
+            $table->string('numero_cedula');
+            $table->string('numero_telefono');
+            $table->string('terminos_pagos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proveedores');
+    }
+}
